b0fa96e8c2ad42c481028683df558caca8e560c1
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 19:11:14 +0800

    currency conversion

--- mapCurrencyConverter.go
+++ mapCurrencyConverter.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //Declare Stuct here
 type currency struct {
@@ -16,10 +18,24 @@ func main() {
 	fmt.Println(m)
 	// map[code:{currencyName rate}]
 	m["USD"] = currency{"US dollar", 1.1318}
-	m["USD"] = currency{"Japanese yen", 121.05}
+	m["JPY"] = currency{"Japanese yen", 121.05}
 	m["GBP"] = currency{"Pound Sterling", 0.90630}
 	m["CNY"] = currency{"Chinese yuan renminbi", 7.9944}
 	m["SGD"] = currency{"Singapore dollar", 1.5743}
 	m["MYR"] = currency{"Malaysian ringgit", 4.8390}
 	fmt.Println(m)
+
+	var currencyFrom string
+	var currencyAmount float64
+	var currencyTo string
+	fmt.Println("Enter your currency to convert from: ")
+	fmt.Scanln(&currencyFrom)
+	fmt.Println("Enter your currency amount: ")
+	fmt.Scanln(&currencyAmount)
+	fmt.Println("Enter the currency to convert to: ")
+	fmt.Scanln(&currencyTo)
+
+	value := (currencyAmount / m[currencyFrom].rate) * m[currencyTo].rate
+
+	fmt.Println(value)
 }
