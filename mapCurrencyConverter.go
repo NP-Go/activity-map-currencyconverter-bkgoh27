dcc133279e8b1019a6f4b5a591462d9eb64153e4
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 19:04:00 +0800

    map creation and initialize all keys

--- mapCurrencyConverter.go
+++ mapCurrencyConverter.go
@@ -1,9 +1,25 @@
 package main
 
+import "fmt"
+
 //Declare Stuct here
+type currency struct {
+	currencyName string
+	rate         float64
+}
 
 //Declare Map here
 
 func main() {
-	//Insert Main Code here
+	// map[]
+	m := make(map[string]currency)
+	fmt.Println(m)
+	// map[code:{currencyName rate}]
+	m["USD"] = currency{"US dollar", 1.1318}
+	m["USD"] = currency{"Japanese yen", 121.05}
+	m["GBP"] = currency{"Pound Sterling", 0.90630}
+	m["CNY"] = currency{"Chinese yuan renminbi", 7.9944}
+	m["SGD"] = currency{"Singapore dollar", 1.5743}
+	m["MYR"] = currency{"Malaysian ringgit", 4.8390}
+	fmt.Println(m)
 }
